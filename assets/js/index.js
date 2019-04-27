6d354a937abddb4482099e67aa19bf8776503d62
Harmangrewal8 <46337286+Harmangrewal8@users.noreply.github.com>
2019-04-27 13:48:17 -0700

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+function displayMenu() {
+	var x = document.getElementById("nav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+$alert("working");
